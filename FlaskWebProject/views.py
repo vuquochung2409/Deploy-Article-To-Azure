a5a6a1c40cb860504bee7ace53a2035287568ae6
vuquochung2409 <vuquochung2409@gmail.com>
2024-02-22 19:13:55 +0700

    fix bug 2

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -86,7 +86,7 @@ def authorized():
     if request.args.get('code'):
         cache = _load_cache()
         result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
-          code=request.args['code'],
+          request.args['code'],
           scopes=Config.SCOPE,
           redirect_uri=url_for('authorized', _external=True, _scheme="https"))
         if "error" in result:
